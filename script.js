61d33c29276ec35a2c1187ca2dc9621cc45d64be
Ian Zolitor <ianzolitor@gmail.com>
2017-05-26 14:41:31 -0400

    results rendered

--- script.js
+++ script.js
@@ -6,27 +6,44 @@ class Search extends React.Component {
   constructor(props) {
     super(props)
     this.state={
-      brand: "",
-      model: "",
-      year: ""
+      term: "",
+      filtered: []
 
     }
     this.showResults = this.showResults.bind(this);
+    // this.filterResults = this.filterResults.bind(this);
   }
     render() {
+      let filtered = this.state.filtered.map(function(filters){
+        return <div>{filters.brand} {filters.model} year: {filters.year} ${filters.price}</div>
+      })
       return (
         <div>
-
-            <div className = "search-bar">Search Brand<input type="search" onChange = {this.showResults} value={this.state.brand} /> </div>
-            <div className = "type-results">{this.state.brand}</div>
+            <div className = "search-bar">Search Brand<input type="search" onChange = {this.showResults} value={this.state.term} /> </div>
+            <div>{filtered}</div>
         </div>
   )
 
 
     }
 
-    showResults (event) {
-      this.setState({brand: event.target.value})
+    showResults(event) {
+      let searchTerm = event.target.value
+
+      this.setState({term: searchTerm})
+      // setTimeut(function(){
+      //   searchTerm = this.state.term
+      // },100)
+
+      let results = cars.filter(function(car){
+
+      return car.brand===searchTerm || car.model===searchTerm || car.year===searchTerm
+
+
+
+      })
+      this.setState({filtered: results})
+
     }
 }
 
@@ -38,7 +55,7 @@ ReactDOM.render(
 
 
 
-let car = [
+let cars = [
 {
     brand: "Honda",
     model: "Civic",
